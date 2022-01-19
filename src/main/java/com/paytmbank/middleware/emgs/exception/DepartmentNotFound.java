d41192851f9e15d3231c126641b08f290068ce73
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2022-01-19 18:41:04 +0530

    Added Department not found exception class

---
+++ src/main/java/com/paytmbank/middleware/emgs/exception/DepartmentNotFound.java
@@ -0,0 +1,7 @@
+package com.paytmbank.middleware.emgs.exception;
+
+public class DepartmentNotFound extends Exception {
+    public DepartmentNotFound(String message) {
+        super(message);
+    }
+}
