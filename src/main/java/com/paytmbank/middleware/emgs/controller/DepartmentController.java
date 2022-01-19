0df8b475d3c9adc5c5ac7b1649939087cfc04089
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2022-01-19 16:09:14 +0530

    Added department rest controller

--- src/main/java/com/paytmbank/middleware/emgs/controller/DepartmentController.java
+++ src/main/java/com/paytmbank/middleware/emgs/controller/DepartmentController.java
@@ -1,2 +1,11 @@
-package com.paytmbank.middleware.emgs.controller;public class DepartmentController {
+package com.paytmbank.middleware.emgs.controller;
+
+import com.paytmbank.middleware.emgs.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DepartmentController {
+    @Autowired
+    DepartmentService
 }
