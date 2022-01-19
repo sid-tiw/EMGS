0ccac070e50b4653c29bd06d972af9d389596a3b
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2022-01-19 10:54:01 +0530

    Initialized the application

---
+++ src/main/java/com/paytmbank/middleware/emgs/EmgsApplication.java
@@ -0,0 +1,13 @@
+package com.paytmbank.middleware.emgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmgsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paytmbank/middleware/emgs/EmgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paytmbank.middleware.emgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
