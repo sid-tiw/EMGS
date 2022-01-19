e4abfd8da2efc384e4ff9b0adf061001c082a9eb
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2022-01-19 16:20:23 +0530

    Added department controller file. No methods for now.

--- src/main/java/com/paytmbank/middleware/emgs/controller/DepartmentController.java
+++ src/main/java/com/paytmbank/middleware/emgs/controller/DepartmentController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DepartmentController {
-    @Autowired
-    DepartmentService
+//    @Autowired
+//    DepartmentService
 }
