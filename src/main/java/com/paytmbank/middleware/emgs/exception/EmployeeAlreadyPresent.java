6437c8665b1ec595941c25207fba64c94206ff25
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2022-01-19 15:55:45 +0530

    NoArgsConstructor and RequiredArgsConstructor were causing error.

--- src/main/java/com/paytmbank/middleware/emgs/exception/EmployeeAlreadyPresent.java
+++ src/main/java/com/paytmbank/middleware/emgs/exception/EmployeeAlreadyPresent.java
@@ -2,7 +2,6 @@ package com.paytmbank.middleware.emgs.exception;
 
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 public class EmployeeAlreadyPresent extends Exception {
     public EmployeeAlreadyPresent(String message) {
         super(message);
