e93e2f455b5d4e8af1c7222a7101f1c1c92912a1
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2022-01-19 12:03:52 +0530

    Added the role entity

---
+++ src/main/java/com/paytmbank/middleware/emgs/entity/Role.java
@@ -0,0 +1,28 @@
+package com.paytmbank.middleware.emgs.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+public class Role {
+	@Id
+	public String rid;
+	public String name;
+
+	/* Role Description. */
+	/* The size of desc might be more than the maximum size allowed. Check back here if any error occurs. */
+	public String desc;
+
+	/* List of all the employees in the given role. */
+	@OneToMany(mappedBy = "rid")
+	List<Employee> emps;
+}
