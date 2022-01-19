3beb4d2ed64b8826509dc22a43053d86af667cc7
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2022-01-19 11:27:39 +0530

    Added the Employee entity

---
+++ src/main/java/com/paytmbank/middleware/emgs/entity/Employee.java
@@ -0,0 +1,45 @@
+package com.paytmbank.middleware.emgs.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+public class Employee {
+	@Id
+	private String eid;
+	private String email;
+	private String phone;
+	private String fname, sname;
+	
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name = "dpt")
+	private Department dpt;
+
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name = "pid")
+	private Project pid;
+
+	@JsonIgnore
+	@OneToOne
+	@JoinColumn(name = "lid")
+	private Leave lid;
+
+	@JsonIgnore
+	@OneToOne
+	@JoinColummn(name = "rid")
+	private Role rid;
+}
