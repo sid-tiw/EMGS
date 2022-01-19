e9f97f699481e02abc4377f7e2d5839123403088
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2022-01-19 11:32:47 +0530

    Added the Department Entity

---
+++ src/main/java/com/paytmbank/middleware/emgs/entity/Department.java
@@ -0,0 +1,23 @@
+package com.paytmbank.middleware.emgs.entity;
+
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+public class Department {
+	@Id
+	public String deptID;
+	public String name;
+	public String desc; // description of the depratment
+
+	@OneToMany(mappedBy = "dpt")
+	List<Employee> emps;
+}
