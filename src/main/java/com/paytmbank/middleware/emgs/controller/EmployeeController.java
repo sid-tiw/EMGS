936c346927f424f18daa500862984445dbf34a2c
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2022-01-20 10:04:13 +0530

    Added pageable results while accessing, and get by phone and email methods.

--- src/main/java/com/paytmbank/middleware/emgs/controller/EmployeeController.java
+++ src/main/java/com/paytmbank/middleware/emgs/controller/EmployeeController.java
@@ -6,11 +6,9 @@ import com.paytmbank.middleware.emgs.service.EmployeeService;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.net.http.HttpResponse;
@@ -24,23 +22,71 @@ public class EmployeeController {
     /* Although this will be available to the super_admin during production (if any such thing happens). */
     @GetMapping("/users")
     public ResponseEntity<?> getUsers() {
-        return ResponseEntity.ok().body(employeeService.listAll());
+        Page<Employee> pg = employeeService.listAll();
+        return ResponseEntity.ok().body(pg);
     }
 
     /* As the name suggests, it will create a new employee, with validity tests that will take place in the service class */
     @PostMapping("/createEmployee")
     public ResponseEntity<?> createNewEmployee(@RequestBody Employee emp) {
+        /* Basic returnable object, detailing the error and status. */
         EmployeeDetailsBasic obj = new EmployeeDetailsBasic();
         try {
             employeeService.create(emp);
         } catch (Exception e) {
-            obj.setStatus("Failed!");
+            obj.setStatus("Failure!");
             obj.setErrorDesc(e.getLocalizedMessage());
 
             return ResponseEntity.badRequest().body(obj);
         }
+        /* If the check is successful then return the basic details of the empolyee */
         obj = new EmployeeDetailsBasic(emp.getEid(), emp.getFname(), emp.getSname());
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    /* Get Employee by its id. The id parameter maps to eid. */
+    @GetMapping("/getEmployee/{id}")
+    public ResponseEntity<?> getEmployee(@PathVariable(name = "id", required = true) String id) {
+        EmployeeDetailsBasic obj = new EmployeeDetailsBasic();
+        try {
+            Employee emp = employeeService.find(id);
+            return ResponseEntity.ok().body(emp);
+        } catch(Exception e) {
+            obj.setStatus("Failure!");
+            obj.setErrorDesc(e.getLocalizedMessage());
+
+            return ResponseEntity.badRequest().body(obj);
+        }
+    }
+
+    /* Get Employee by its phone number. The phone parameter maps to Employee.phone. */
+    @GetMapping("/getEmployeeByPhone/{phone}")
+    public ResponseEntity<?> getEmployeeByPhone(@PathVariable(name = "phone", required = true) String id) {
+        EmployeeDetailsBasic obj = new EmployeeDetailsBasic();
+        try {
+            Employee emp = employeeService.findByPhone(id);
+            return ResponseEntity.ok().body(emp);
+        } catch(Exception e) {
+            obj.setStatus("Failure!");
+            obj.setErrorDesc(e.getLocalizedMessage());
+
+            return ResponseEntity.badRequest().body(obj);
+        }
+    }
+
+    /* Get Employee by its email. The email parameter maps to Employee.email. */
+    @GetMapping("/getEmployeeByEmail/{email}")
+    public ResponseEntity<?> getEmployeeByEmail(@PathVariable(name = "email", required = true) String id) {
+        EmployeeDetailsBasic obj = new EmployeeDetailsBasic();
+        try {
+            Employee emp = employeeService.findByEmail(id);
+            return ResponseEntity.ok().body(emp);
+        } catch(Exception e) {
+            obj.setStatus("Failure!");
+            obj.setErrorDesc(e.getLocalizedMessage());
+
+            return ResponseEntity.badRequest().body(obj);
+        }
     }
 }
