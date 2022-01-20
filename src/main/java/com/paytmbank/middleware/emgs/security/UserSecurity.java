874def76ba31f861d0babd8d2a3c667701d1db50
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2022-01-20 23:21:40 +0530

    Created UserDetails for security

---
+++ src/main/java/com/paytmbank/middleware/emgs/security/UserSecurity.java
@@ -0,0 +1,56 @@
+package com.paytmbank.middleware.emgs.security;
+
+import com.paytmbank.middleware.emgs.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class UserSecurity implements UserDetails {
+    private Employee user;
+    public UserSecurity(Employee user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(user.getRole()));
+//        List<SimpleGrantedAuthority> grantedAuthorityList = new ArrayList<SimpleGrantedAuthority>();
+//        grantedAuthorityList.add(new SimpleGrantedAuthority(user.getRole()));
+//        return grantedAuthorityList;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEid();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return user.isActive();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return user.isActive();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return user.isActive();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isActive();
+    }
+}
