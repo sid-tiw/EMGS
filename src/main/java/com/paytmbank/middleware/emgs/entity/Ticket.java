31c2f794e49ce0d53d10c750daf31877c0e0742c
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2022-01-21 11:37:05 +0530

    */Ticket.java

---
+++ src/main/java/com/paytmbank/middleware/emgs/entity/Ticket.java
@@ -0,0 +1,26 @@
+package com.paytmbank.middleware.emgs.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.minidev.json.annotate.JsonIgnore;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Getter @Setter
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String description;
+    private Timestamp createdDate;
+    private Timestamp resolvedDate;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "raisedBy")
+    private Employee raisedBy;
+}
